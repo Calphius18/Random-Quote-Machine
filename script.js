fa2b7e848256a493644f551a771bc8fe7e75931e
Femi <fagbemibabs18@gmail.com>
2023-09-18 08:59:54 +0100

    Quote

---
+++ script.js
@@ -0,0 +1,286 @@
+var quotes =[
+    { 
+      text: "Dude, suckin' at something is the first step at being sorta good at something.", 
+      author: "Jake The Dog",
+      img: "https://i.kym-cdn.com/photos/images/newsfeed/001/141/641/6dc.gif"
+    },
+    { 
+      text: "Dreams are meaningful when you work toward them in the real world. If you merely live within the dreams of other people, it's no different from being dead.", 
+      author: "Motoko Kusanagi",
+      img: "https://s-media-cache-ak0.pinimg.com/originals/36/e3/b1/36e3b16f35a6b6109686ac806b2fccc1.jpg"
+    },
+    { 
+      text: "Either I will find a way, or I will make one.", 
+      author: "Philip Sidney",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Sir_Philip_Sidney_from_NPG.jpg/220px-Sir_Philip_Sidney_from_NPG.jpg"
+    },
+    { 
+      text: "If you can dream it, you can achieve it.", 
+      author: "Zig Ziglar",
+      img: "https://moneyexcel.com/wp-content/uploads/2015/04/zig-ziglar-quote.jpg"
+    },
+     { 
+      text: "The only way to do great work is to love what you do.", 
+      author: "Steve Jobs",
+      img: "https://media3.giphy.com/media/TfelnmQ8VU3K/giphy.gif?cid=ecf05e47xeg9zr1v7iz4c80j2577v9ncvb7jpriw71oaqou6&rid=giphy.gif&ct=g"
+    }, 
+    { 
+      text: "Nothing is impossible, the word itself says, “I’m possible!”", 
+      author: "Audrey Hepburn",
+      img: "https://quotestoenjoy.com/wp-content/uploads/bfi_thumb/nothing-is-impossible-the-word-itself-says-i-m-possible-3dvfbmkzj40cn45nvh4r9c.jpg"
+    },
+     { 
+      text: "Either write something worth reading or do something worth writing.", 
+      author: "Benjamin Franklin",
+      img: "https://assets.editorial.aetnd.com/uploads/2009/11/benjamin-franklin-credit-fineart-alamy-stock.jpg?width=768"
+    },
+     { 
+      text: "Change your thoughts and you change your world.", 
+      author: "Norman Vincent Peale",
+      img: "https://succeedfeed.com/wp-content/uploads/2017/12/77-norman-vincent-peale-quotes.jpg"
+    },
+     { 
+      text: "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.", 
+      author: "Henry Ford",
+      img: "https://lh3.googleusercontent.com/IzbKfdthcqXlOgOy7Oysh_3C7jVVUhaA0sz73p4RoiM70CT7yVVvedT8t-TMZWJylfGkPwRfm0QGZoQSHpNQDa3wFPWqqx96_vq84g=w600"
+    },
+     { 
+      text: "The question isn’t who is going to let me; it’s who is going to stop me.", 
+      author: "Ayn Rand",
+      img: "https://64.media.tumblr.com/6243cf4b93fd23753e982c64769c8b3a/tumblr_p58nh5NRbJ1ru8yv8o1_540.gifv"
+    },
+     { 
+      text: "You may be disappointed if you fail, but you are doomed if you don’t try.", 
+      author: "Beverly Sills",
+      img: "https://www.azquotes.com/picture-quotes/quote-you-may-be-disappointed-if-you-fail-but-you-are-doomed-if-you-don-t-try-beverly-sills-27-15-92.jpg"
+    }, 
+    { 
+      text: "If you do what you’ve always done, you’ll get what you’ve always gotten.", 
+      author: "Tony Robbins",
+      img: "https://www.incimages.com/uploaded_files/image/336x336/TRobbins_44124.jpg"
+    },
+     { 
+      text: "Do what you can, where you are, with what you have.", 
+      author: "Theodore Roosevelt",
+      img: "https://i.pinimg.com/736x/b1/5e/9a/b15e9a3c82021e9784d895fa9d477603.jpg"
+    },
+     { 
+      text: "It does not matter how slowly you go as long as you do not stop.", 
+      author: "Confucius",
+      img: "https://www.azquotes.com/picture-quotes/quote-it-does-not-matter-how-slowly-you-go-as-long-as-you-do-not-stop-confucius-6-21-25.jpg"
+    },
+     { 
+      text: "Build your own dreams, or someone else will hire you to build theirs.", 
+      author: "Farrah Gray",
+      img: "https://www.azquotes.com/picture-quotes/quote-you-know-you-don-t-have-to-have-money-to-be-a-successful-businessperson-you-don-t-need-farrah-gray-35-41-81.jpg"
+    },
+     { 
+      text: "A truly rich man is one whose children run into his arms when his hands are empty.", 
+      author: "Unknown",
+      img: "https://i.imgur.com/IstONkO.gif"
+    },
+     { 
+      text: "I would rather die of passion than of boredom.", 
+      author: "Vincent Van Gogh",
+      img: "https://c.tenor.com/TLBoeqXBJ7gAAAAd/loving-vincent-paint.gif"
+    },
+     { 
+      text: "It is never too late to be what you might have been.", 
+      author: "George Eliot",
+      img: "https://www.azquotes.com/picture-quotes/quote-it-is-never-too-late-to-become-the-person-you-always-thought-you-could-be-george-eliot-140-32-13.jpg"
+    },
+     { 
+      text: "There are no traffic jams along the extra mile.", 
+      author: "Roger Staubach",
+      img: "https://www.heisman.com/wp-content/uploads/2018/05/1963-Staubach-Roger.jpg?x27196"
+    },
+    { 
+      text: "The person who says it cannot be done should not interrupt the person who is doing it.", 
+      author: "Chinese Proverb",
+      img: "https://downloadhdwallpapers.in/wp-content/uploads/2018/06/China-Flag-Waving-Animated-Gif-Cute-.gif"
+    },
+    { 
+      text: "A person who never made a mistake never tried anything new.", 
+      author: "Albert Einstein",
+      img: "https://i.pinimg.com/originals/22/d5/2e/22d52eb2d0fad3f404e5576c96512988.gif"
+    },
+    { 
+      text: "I didn’t fail the test. I just found 100 ways to do it wrong.", 
+      author: "Benjamin Franklin",
+      img: "https://assets.editorial.aetnd.com/uploads/2009/11/benjamin-franklin-credit-fineart-alamy-stock.jpg?width=768"
+    },
+    { 
+      text: "Challenges are what make life interesting and overcoming them is what makes life meaningful.", 
+      author: "Joshua J. Marine",
+      img: "https://i.pinimg.com/736x/1d/03/94/1d0394503d980f1ad164b43edd86762b.jpg"
+    },
+    { 
+      text: "You can’t fall if you don’t climb.  But there’s no joy in living your whole life on the ground.", 
+      author: "Unknown",
+      img: "https://i.imgur.com/IstONkO.gif"
+    },
+    { 
+      text: "We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained.", 
+      author: "Marie Curie",
+      img: "https://www.azquotes.com/vangogh-image-quotes/6/92/Quotation-Marie-Curie-Life-is-not-easy-for-any-of-us-But-what-6-92-90.jpg"
+    },
+      { 
+      text: "When I was 5 years old, my mother always told me that happiness was the key to life.  When I went to school, they asked me what I wanted to be when I grew up.  I wrote down ‘happy’.  They told me I didn’t understand the assignment, and I told them they didn’t understand life.", 
+      author: "John Lennon",
+      img: "https://www.azquotes.com/picture-quotes/quote-when-i-was-5-years-old-my-mother-always-told-me-that-happiness-was-the-key-to-life-when-john-lennon-41-63-17.jpg"
+    },
+      { 
+      text: "We can easily forgive a child who is afraid of the dark; the real tragedy of life is when men are afraid of the light.", 
+      author: "Plato",
+      img: "https://philosophybreak.com/static/802cdfcc0384c32062cbd38a24c7c547/5267c/socrates.jpg"
+    },
+     { 
+      text: "I am the wisest man alive, for I know one thing, and that is that I know nothing.", 
+      author: "Plato",
+      img: "https://www.azquotes.com/picture-quotes/quote-i-am-the-wisest-man-alive-for-i-know-one-thing-and-that-is-that-i-know-nothing-plato-66-86-46.jpg"
+    },
+      { 
+      text: "Certain things catch your eye, but pursue only those that capture the heart.", 
+      author: "Ancient Indian Proverb",
+      img: "https://wallpapers.com/images/hd/indian-flag-on-face-painting-tl2f6v7n4x5c4dy7.webp"
+    },
+     { 
+      text: "When I stand before God at the end of my life, I would hope that I would not have a single bit of talent left and could say, I used everything you gave me.", 
+      author: "Erma Bombeck",
+      img: "https://www.azquotes.com/picture-quotes/quote-when-i-stand-before-god-at-the-end-of-my-life-i-would-hope-that-i-would-not-have-a-single-erma-bombeck-3-11-68.jpg"
+    },
+     { 
+      text: "Worry is like a rocking chair; it gives you something to do but never get you anywhere", 
+      author: "Erma Bombeck",
+      img: "https://c.tenor.com/e7M1eh_5VEcAAAAC/worrying-rocking-chair.gif"
+    },
+     { 
+      text: "The only person you are destined to become is the person you decide to be.", 
+      author: "Ralph Waldo Emerson",
+      img: "https://www.azquotes.com/picture-quotes/quote-the-only-person-you-are-destined-to-become-is-the-person-you-decide-to-be-ralph-waldo-emerson-36-35-17.jpg"
+    },
+      { 
+      text: "Don't waste life in doubts and fears; spend yourself on the work before you, well assured that the right performance of this hour's duties will be the best preparation for the hours and ages that will follow it.", 
+      author: "Ralph Waldo Emerson",
+      img: "https://www.azquotes.com/picture-quotes/quote-don-t-waste-life-in-doubts-and-fears-spend-yourself-on-the-work-before-you-well-assured-ralph-waldo-emerson-52-28-27.jpg"
+    },
+     { 
+      text: "Ask and it will be given to you; search, and you will find; knock and the door will be opened for you.", 
+      author: "Jesus",
+      img: "http://iliveforjesus.in/wp-content/uploads/2018/07/ask-seek-knock.jpeg"
+    },
+     { 
+      text: "The two most important days in your life are the day you are born and the day you find out why.", 
+      author: "Mark Twain",
+      img: "https://www.azquotes.com/picture-quotes/quote-the-two-most-important-days-in-your-life-are-the-day-you-are-born-and-the-day-you-find-mark-twain-47-14-64.jpg"
+    },
+     { 
+      text: "I’ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.", 
+      author: "Maya Angelou",
+      img: "https://www.azquotes.com/picture-quotes/quote-i-ve-learned-that-people-will-forget-what-you-said-people-will-forget-what-you-did-but-maya-angelou-0-84-85.jpg"
+    },
+     { 
+      text: "You can never cross the ocean until you have the courage to lose sight of the shore.", 
+      author: "Christopher Columbus",
+      img: "https://cdn.cdnparenting.com/articles/2021/03/01190401/1191804934.jpg"
+    },
+      { 
+      text: "It is easy to discover what another has discovered before", 
+      author: "Christopher Columbus",
+      img: "https://www.inspiringquotes.us/image/author/25-Famous-Christopher-Columbus-Quotes-Sayingimages-Com-Christopher-Columbus-Quotes-Christopher-Columbus-Columbus.jpg?ezimgfmt=rs:382x320/rscb4/ngcb4/notWebP"
+    },
+     { 
+      text: "Winning isn’t everything, but wanting to win is.", 
+      author: "Vincent Lombardi",
+      img: "https://www.azquotes.com/picture-quotes/quote-winning-is-not-everything-but-wanting-to-win-is-vince-lombardi-17-80-99.jpg"
+    },
+     { 
+      text: "The best time to plant a tree was 20 years ago. The second best time is now.", 
+      author: "Chinese Proverb",
+      img: "https://downloadhdwallpapers.in/wp-content/uploads/2018/06/China-Flag-Waving-Animated-Gif-Cute-.gif"
+    },
+     { 
+      text: "The mind is everything. What you think you become.", 
+      author: "Buddha",
+      img: "https://media.istockphoto.com/photos/buddha-statue-picture-id485020819?k=20&m=485020819&s=612x612&w=0&h=JhajfqdWefeLIIdQfySXd54OIdCOqzc8edBcx-ocPlg="
+    },
+     { 
+      text: "The most difficult thing is the decision to act, the rest is merely tenacity.", 
+      author: "Amelia Earhart",
+      img: "https://64.media.tumblr.com/73fcca098ffbd35626ec25bf746bf2cd/tumblr_ob16vbF9Nx1u9srq1o1_500.gifv"
+    },
+     { 
+      text: "I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.", 
+      author: "Michael Jordan",
+      img: "https://c.tenor.com/aY4wOLYFNZYAAAAd/nba-jordan.gif"
+    },
+     { 
+      text: "You miss 100% of the shots you don’t take.", 
+      author: "Wayne Gretzky",
+      img: "https://i.pinimg.com/originals/20/d4/b9/20d4b95b300d963366b782666c6af80b.jpg"
+    },
+     { 
+      text: "Whatever the mind of man can conceive and believe, it can achieve.", 
+      author: "Oliver Napolean Hill",
+      img: "https://www.azquotes.com/picture-quotes/quote-whatever-the-mind-of-man-can-conceive-and-believe-it-can-achieve-thoughts-are-things-napoleon-hill-138-81-14.jpg"
+    },
+     { 
+      text: "If you win, you need not have to explain...If you lose, you should not be there to explain!", 
+      author: "Adolf Hitler",
+      img: "https://m.media-amazon.com/images/M/MV5BMTMwMmI5NTUtMWFiZi00YjI5LWJhZDQtMTA5MzhkNWNjMmFkXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg"
+    },
+     { 
+      text: "Do not compare yourself to others. If you do so, you are insulting yourself.", 
+      author: "Adolf Hitler",
+      img: "https://m.media-amazon.com/images/M/MV5BMTMwMmI5NTUtMWFiZi00YjI5LWJhZDQtMTA5MzhkNWNjMmFkXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg"
+    },
+     { 
+      text: "If you want to shine like sun first you have to burn like it.", 
+      author: "Adolf Hitler",
+      img: "https://m.media-amazon.com/images/M/MV5BMTMwMmI5NTUtMWFiZi00YjI5LWJhZDQtMTA5MzhkNWNjMmFkXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg"
+    },
+     { 
+      text: "Anyone can deal with victory. Only the mighty can bear defeat.", 
+      author: "Adolf Hitler",
+      img: "https://m.media-amazon.com/images/M/MV5BMTMwMmI5NTUtMWFiZi00YjI5LWJhZDQtMTA5MzhkNWNjMmFkXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_.jpg"
+    },
+     { 
+      text: "Find something you love and do it better than everyone else.", 
+      author: "Gurbaksh Chahal",
+      img: "https://assets.entrepreneur.com/article/gurbaksh-chahal-clickagents.jpg"
+    },
+     { 
+      text: "Don't Walk like you own the World, Walk like you don't care who Does.", 
+      author: "Gurbaksh Chahal",
+      img: "https://assets.entrepreneur.com/article/gurbaksh-chahal-clickagents.jpg"
+    }
+    
+  ];
+  var currentQuote = "",
+    currentAuthor = "";
+  var getNewQuote = function() {
+     var text = quotes[Math.floor(Math.random() * quotes.length)];
+        document.getElementById("text" ).innerHTML =
+          '<img src="' + text.img + '">' +
+          '<h5>' + text.text + '</h5>';
+    
+    document.getElementById("author").innerHTML =
+       '<p>' + '-' + '<em>' + text.author + '</em>' + '</p>';
+  };
+  
+  var tumblrTheQuote = function() {
+      currentQuote = text.text;
+    currentAuthor = text.author;
+    
+  window.open("https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
+     encodeURIComponent(currentAuthor) +
+        '&content=' +
+        encodeURIComponent(currentQuote) +   '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareauthor=tumblr_share_button'
+  );
+  }
+  
+  
+  window.onload=getNewQuote();
+  
+  
+  
